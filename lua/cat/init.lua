4c91cba8a64d97f4e48e6f0325cdca1c87795766
CatDoesCode <catdoescode@gmail.com>
2025-04-29 14:30:17 -0400

    Initial Setup

---
+++ lua/cat/init.lua
@@ -0,0 +1,11 @@
+require("cat.remap")
+require("cat.set")
+
+require("cat.lazy")
+
+require("cat.theme")
+require("cat.lsp")
+
+
+vim.diagnostic.config({ virtual_text = true })
+
---
+++ lua/cat/theme.lua
@@ -0,0 +1,16 @@
+require("gruvbox").setup({
+  terminal_colors = true, -- add neovim terminal colors
+  undercurl = true,
+  underline = true,
+  bold = true,
+  italic = {
+    strings = false,
+    emphasis = true,
+    comments = true,
+    operators = false,
+    folds = true,
+  }
+})
+
+vim.o.background = "dark"
+vim.cmd([[colorscheme gruvbox]])
